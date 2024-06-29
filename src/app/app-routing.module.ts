7a972ec35f4dc16ae5df05c648e70bd4af06dbb8
Davi Milioli <davimilioli2108@gmail.com>
2024-06-29 11:04:37 -0300

    criação de páginas e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: AboutComponent
+  },
+  {
+    path: 'habilidades',
+    component: SkillsComponent
+  },
+  {
+    path: 'projetos',
+    component: ProjectsComponent
+  },
+  {
+    path: 'contato',
+    component: ContactComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
