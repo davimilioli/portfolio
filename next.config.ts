a877dabb1ca418fb70827a7647d9390c6637e492
Davi Milioli <davimilioli2108@gmail.com>
2026-06-15 18:29:01 -0300

    feat: configuração de suporte a arquivos MDX no Next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
+    pageExtensions: ['ts', 'tsx', 'mdx'],
     /* config options here */
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    options: {},
+})
+
+export default withMDX(nextConfig)
